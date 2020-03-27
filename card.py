3a72e3d5f322f77d1feffaaf4c6a6154e84aa533
Hristiyana <hpmalinova@github.com>
2020-03-27 21:11:26 +0200

    Rank and Suit as class members

--- card.py
+++ card.py
@@ -2,17 +2,19 @@
 
 class Card:
 	# Constructor
+	ranks = ['7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+	suits = ['hearts', 'spades', 'clubs', 'diamonds']
+	
+	def __init__(self, rank, suit):
+		self.validate_card(rank, suit)
 
-	def __init__(self, number, suit):
-		self.validate_card(number, suit)
-
-		self.__number = number
+		self.__rank = rank
 		self.__suit = suit
 
 	# Getters
 
-	def get_number(self):
-		return self.__number
+	def get_rank(self):
+		return self.__rank
 
 	def get_suit(self):
 		return self.__suit	
@@ -29,32 +31,28 @@ class Card:
 	def __string_repr(self):
 		dictionary = {'hearts': u'\u2665', 'diamonds': u'\u2666', 'clubs': u'\u2663', 'spades': u'\u2660'}
 		
-		return self.__number + dictionary[self.__suit]
+		return self.__rank + dictionary[self.__suit]
 
 	def __eq__(self, other):
-		return self.__number == other.__number and self.__suit == other.__suit
+		return self.__rank == other.__rank and self.__suit == other.__suit
 
 	def __int__(self):
 		int_value_of_cards = {'7' : 7, '8': 8, '9': 9, '10': 10,'J': 11, 'Q': 12, 'K': 13, 'A':14}
 
-		return int(int_value_of_cards[self.__number])
+		return int(int_value_of_cards[self.__rank])
 
 	def __lt__(self, other):
 		return int(self) < int(other)
 
 
-	# Static
-
-	@staticmethod
-	def validate_card(number, suit):
-		allowed_numbers = ['7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-		allowed_suits = ['hearts', 'spades', 'clubs', 'diamonds']
-
-		if type(number) is not str:
-			raise TypeError('Number must be of "str" type.')
+	# Class Method
+	@classmethod
+	def validate_card(cls, rank, suit):
+		if type(rank) is not str:
+			raise TypeError('Rank must be of "str" type.')
 		elif type(suit) is not str:
 			raise TypeError('Suit must be of "str" type.')
-		elif number not in allowed_numbers:
-			raise Exception('Unrecognized number.')
-		elif suit not in allowed_suits:
+		elif rank not in cls.ranks:
+			raise Exception('Unrecognized rank.')
+		elif suit not in cls.suits:
 			raise Exception('Unrecognized suit.')
--- tests_card.py
+++ tests_card.py
@@ -7,25 +7,25 @@ from card import Card
 
 class TestCardValidateCard(unittest.TestCase):
 	def test_card_validate_card_raises_exception_if_number_type_is_not_str(self):
-		test_number = 5
+		test_rank = 5
 		test_suit = 'diamonds'
 		exc = None
 
 		try:
-			Card.validate_card(test_number, test_suit)
+			Card.validate_card(test_rank, test_suit)
 		except TypeError as err:
 			exc = err
 
 		self.assertIsNotNone(exc)
-		self.assertEqual(str(exc), 'Number must be of "str" type.')
+		self.assertEqual(str(exc), 'Rank must be of "str" type.')
 
 	def test_card_validate_card_raises_exception_if_suit_type_is_not_str(self):
-		test_number = '9'
+		test_rank = '9'
 		test_suit = 3
 		exc = None
 
 		try:
-			Card.validate_card(test_number, test_suit)
+			Card.validate_card(test_rank, test_suit)
 		except TypeError as err:
 			exc = err
 
@@ -33,12 +33,12 @@ class TestCardValidateCard(unittest.TestCase):
 		self.assertEqual(str(exc), 'Suit must be of "str" type.')
 
 	def test_card_validate_card_raises_exception_if_suit_is_not_allowed(self):
-		test_number = '8'
+		test_rank = '8'
 		test_suit = 'p'
 		exc = None
 
 		try:
-			Card.validate_card(test_number, test_suit)
+			Card.validate_card(test_rank, test_suit)
 		except Exception as err:
 			exc = err
 
@@ -46,87 +46,87 @@ class TestCardValidateCard(unittest.TestCase):
 		self.assertEqual(str(exc), 'Unrecognized suit.')
 
 	def test_card_validate_card_raises_exception_if_number_is_not_allowed(self):
-		test_number = '12'
+		test_rank = '12'
 		test_suit = 'diamonds'
 		exc = None
 
 		try:
-			Card.validate_card(test_number, test_suit)
+			Card.validate_card(test_rank, test_suit)
 		except Exception as err:
 			exc = err
 
 		self.assertIsNotNone(exc)
-		self.assertEqual(str(exc), 'Unrecognized number.')
+		self.assertEqual(str(exc), 'Unrecognized rank.')
 
 	def test_card_validate_card_passes_with_correct_input(self):
-		test_number = 'A'
+		test_rank = 'A'
 		test_suit = 'diamonds'
 
-		test_obj = Card(test_number, test_suit)
+		test_obj = Card(test_rank, test_suit)
 
 class TestCardInit(unittest.TestCase):
 	def test_card_init_initializes_object_as_expected(self):
-		test_number = '8'
+		test_rank = '8'
 		test_suit = 'hearts'
-		test_obj = Card(test_number, test_suit)
+		test_obj = Card(test_rank, test_suit)
 
-		self.assertEqual(test_obj.get_number(), '8')
+		self.assertEqual(test_obj.get_rank(), '8')
 		self.assertEqual(test_obj.get_suit(), 'hearts')
 
 class TestCardStrDunder(unittest.TestCase):
 	def test_card_str_representation_is_as_expected(self):
-		test_number = '8'
+		test_rank = '8'
 		test_suit = 'hearts'
-		test_obj = Card(test_number, test_suit)
+		test_obj = Card(test_rank, test_suit)
 
 		self.assertEqual(str(test_obj), '8' + u'\u2665')
 		
 
 class TestCardReprDunder(unittest.TestCase):
 	def test_card_repr_representation_is_as_expected(self):
-		test_number = '8'
+		test_rank = '8'
 		test_suit = 'hearts'
-		test_obj = Card(test_number, test_suit)
+		test_obj = Card(test_rank, test_suit)
 
 		self.assertEqual(repr(test_obj), '8'+ u'\u2665')
 
 class TestCardEqDunder(unittest.TestCase):
 	def test_card_eq_compares_cards_as_expected(self):
-		test_number1 = '8'
+		test_rank1 = '8'
 		test_suit1 = 'hearts'
 
-		test_obj1 = Card(test_number1, test_suit1)
-		test_obj2 = Card(test_number1, test_suit1)
+		test_obj1 = Card(test_rank1, test_suit1)
+		test_obj2 = Card(test_rank1, test_suit1)
 
 		self.assertEqual(test_obj1, test_obj2)
 
-		test_number2 = '7'
+		test_rank2 = '7'
 		test_suit2 = 'diamonds'
 
-		test_obj3 = Card(test_number2, test_suit2)
+		test_obj3 = Card(test_rank2, test_suit2)
 
 		self.assertNotEqual(test_obj1, test_obj3)
 
 class TestCardLtDunder(unittest.TestCase):
 	def test_card_lt_compares_cards_as_expected_when_different_suits(self):
-		test_number1 = '7'
+		test_rank1 = '7'
 		test_suit1 = 'hearts'
-		test_number2 = '8'
+		test_rank2 = '8'
 		test_suit2 = 'diamonds'
 
-		test_obj1 = Card(test_number1, test_suit1)
-		test_obj2 = Card(test_number2, test_suit2)
+		test_obj1 = Card(test_rank1, test_suit1)
+		test_obj2 = Card(test_rank2, test_suit2)
 
 		self.assertLess(test_obj1, test_obj2)
 
 	def test_card_lt_compares_cards_as_expected_same_different_suit(self):
-		test_number1 = '7'
+		test_rank1 = '7'
 		test_suit1 = 'hearts'
-		test_number2 = '8'
+		test_rank2 = '8'
 		test_suit2 = 'hearts'
 
-		test_obj1 = Card(test_number1, test_suit1)
-		test_obj2 = Card(test_number2, test_suit2)
+		test_obj1 = Card(test_rank1, test_suit1)
+		test_obj2 = Card(test_rank2, test_suit2)
 
 		self.assertLess(test_obj1, test_obj2)
 
