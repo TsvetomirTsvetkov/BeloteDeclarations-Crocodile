a0df3b2647954eecf1e452ae92dede3bf765b776
Hristiyana <hpmalinova@github.com>
2020-03-27 20:48:19 +0200

    Add rremove method

--- hand.py
+++ hand.py
@@ -3,13 +3,8 @@ from card import Card
 class Hand:
 	def __init__(self, cards): # cards from type Card 
 		self.__cards = sorted(cards)
-
-		self.__numbers = {'7': 0, '8': 0, '9': 0, '10': 0, 'J': 0, 'Q': 0, 'K': 0, 'A': 0} # how many times we have the card
-		self.__suits = {'hearts': [], 'diamonds': [], 'clubs': [], 'spades': []} # hearts: list of card numbers of that suit
-
-		for card in self.__cards:
-			self.__numbers[card.get_number()] += 1
-			self.__suits[card.get_suit()].append(card.get_number())
+		self.init_dictionaries()
+		self.update_dictionaries()
 
 	def get_cards(self):
 		return self.__cards	
@@ -17,9 +12,29 @@ class Hand:
 	def get_cards_by_suit(self, suit):
 		return self.__suits[suit]
 
-	def get_number_of_cards_with_number(self, number):
-		return self.__numbers[number]
+	def get_number_of_cards_with_rank(self, rank):
+		return self.__ranks[rank]
 
+	def init_dictionaries(self):
+		self.__ranks = {'7': 0, '8': 0, '9': 0, '10': 0, 'J': 0, 'Q': 0, 'K': 0, 'A': 0} # how many times we have the card
+		self.__suits = {'hearts': [], 'diamonds': [], 'clubs': [], 'spades': []} # hearts: list of card numbers of that suit
+
+	def update_dictionaries(self):
+		for card in self.__cards:
+			self.__ranks[card.get_number()] += 1
+			self.__suits[card.get_suit()].append(card.get_number())
+
+	def remove_cards(self, cards_to_remove):
+		new_cards = []
+
+		for card in self.__cards:
+			if card not in cards_to_remove:
+				new_cards.append(card)
+
+		self.__cards = new_cards		
+		self.init_dictionaries()
+		self.update_dictionaries()
+		
 	# TODO
 	# Къде да е тая ф-я? Защото май в Карти не е много подходящо? 
 	@staticmethod 
@@ -31,7 +46,6 @@ class Hand:
 
 		return value							
 
-
 	def __str__(self):
 		return ''.join(str(card) + ' ' for card in self.__cards) 
 
@@ -40,3 +54,6 @@ class Hand:
 	
 	def __getitem__(self, index):
 		return self.__cards[index]	
+
+	def __eq__(self, other):
+		return self.__cards == other.__cards	
--- test_hand.py
+++ test_hand.py
@@ -5,40 +5,77 @@ from hand import Hand
 class TestHand(unittest.TestCase):
 	def setUp(self):
 		cards = [Card('A', 'hearts'), Card('8', 'spades'), Card('J', 'hearts'), Card('Q', 'hearts'), \
-		Card('7', 'hearts'), Card('K', 'hearts'), Card('J', 'spades'), Card('A', 'diamonds')]
+				 Card('7', 'hearts'), Card('K', 'hearts'), Card('J', 'spades'), Card('A', 'diamonds')]
 
 		self.hand = Hand(cards)
 
-	def test_get_cards_from_specific_suit(self):
+	def test_get_number_of_cards_with_specific_rank(self):
+		self.assertEqual(self.hand.get_number_of_cards_with_rank('7'), 1)
+		self.assertEqual(self.hand.get_number_of_cards_with_rank('8'), 1)
+		self.assertEqual(self.hand.get_number_of_cards_with_rank('9'), 0)
+		self.assertEqual(self.hand.get_number_of_cards_with_rank('10'), 0)
+		self.assertEqual(self.hand.get_number_of_cards_with_rank('J'), 2)
+		self.assertEqual(self.hand.get_number_of_cards_with_rank('Q'), 1)
+		self.assertEqual(self.hand.get_number_of_cards_with_rank('K'), 1)
+		self.assertEqual(self.hand.get_number_of_cards_with_rank('A'), 2)
+	
+	def test_get_cards_with_specific_suit(self):
 		self.assertEqual(self.hand.get_cards_by_suit('hearts'), ['7', 'J', 'Q', 'K', 'A'])
 		self.assertEqual(self.hand.get_cards_by_suit('spades'), ['8', 'J'])
 		self.assertEqual(self.hand.get_cards_by_suit('diamonds'), ['A'])
 		self.assertEqual(self.hand.get_cards_by_suit('clubs'), [])
 
-	def test_get_number_of_cards_with_specific_card_number(self):
-		self.assertEqual(self.hand.get_number_of_cards_with_number('7'), 1)
-		self.assertEqual(self.hand.get_number_of_cards_with_number('8'), 1)
-		self.assertEqual(self.hand.get_number_of_cards_with_number('9'), 0)
-		self.assertEqual(self.hand.get_number_of_cards_with_number('10'), 0)
-		self.assertEqual(self.hand.get_number_of_cards_with_number('J'), 2)
-		self.assertEqual(self.hand.get_number_of_cards_with_number('Q'), 1)
-		self.assertEqual(self.hand.get_number_of_cards_with_number('K'), 1)
-		self.assertEqual(self.hand.get_number_of_cards_with_number('A'), 2)
-		
 	def test_init(self):
 		expected_result = [Card('7', 'hearts'),  Card('8', 'spades'), Card('J', 'hearts'), Card('J', 'spades'), \
-		Card('Q', 'hearts'), Card('K', 'hearts'),  Card('A', 'hearts'), Card('A', 'diamonds')]
+						   Card('Q', 'hearts'), Card('K', 'hearts'),  Card('A', 'hearts'), Card('A', 'diamonds')]
 		
 		self.assertEqual(self.hand.get_cards(), expected_result)	
 
-
 	def test_get_str_representation_of_the_hand(self):
 		sorted_cards = [Card('7', 'hearts'),  Card('8', 'spades'), Card('J', 'hearts'), Card('J', 'spades'), \
-		Card('Q', 'hearts'), Card('K', 'hearts'),  Card('A', 'hearts'), Card('A', 'diamonds')]
+						Card('Q', 'hearts'), Card('K', 'hearts'),  Card('A', 'hearts'), Card('A', 'diamonds')]
 
 		expected = ''.join(str(card) + ' ' for card in sorted_cards)
 
 		self.assertEqual(str(self.hand), expected)
+
+	def test_when_remove_cards_then_update_cards(self):
+		cards_to_remove = [Card('J', 'hearts'), Card('8', 'spades')]
+		expected_result = [Card('7', 'hearts'), Card('J', 'spades'), Card('Q', 'hearts'), \
+						   Card('K', 'hearts'),  Card('A', 'hearts'), Card('A', 'diamonds')]
+
+		self.hand.remove_cards(cards_to_remove)
+
+		self.assertEqual(self.hand.get_cards(), expected_result)
+
+	def test_when_remove_cards_then_update_rank_dictionary(self):
+		cards_to_remove = [Card('J', 'hearts'), Card('8', 'spades')]
+		expected_result = [Card('7', 'hearts'), Card('J', 'spades'), Card('Q', 'hearts'), \
+						   Card('K', 'hearts'),  Card('A', 'hearts'), Card('A', 'diamonds')]
+
+		self.hand.remove_cards(cards_to_remove)
+
+		self.assertEqual(self.hand.get_number_of_cards_with_rank('7'), 1)
+		self.assertEqual(self.hand.get_number_of_cards_with_rank('8'), 0)
+		self.assertEqual(self.hand.get_number_of_cards_with_rank('9'), 0)
+		self.assertEqual(self.hand.get_number_of_cards_with_rank('10'), 0)
+		self.assertEqual(self.hand.get_number_of_cards_with_rank('J'), 1)
+		self.assertEqual(self.hand.get_number_of_cards_with_rank('Q'), 1)
+		self.assertEqual(self.hand.get_number_of_cards_with_rank('K'), 1)
+		self.assertEqual(self.hand.get_number_of_cards_with_rank('A'), 2)
+
+	def test_when_remove_cards_then_update_suit_dictionary(self):
+		cards_to_remove = [Card('J', 'hearts'), Card('8', 'spades')]
+		expected_result = [Card('7', 'hearts'), Card('J', 'spades'), Card('Q', 'hearts'), \
+						   Card('K', 'hearts'),  Card('A', 'hearts'), Card('A', 'diamonds')]
+
+		self.hand.remove_cards(cards_to_remove)
+
+		self.assertEqual(self.hand.get_cards_by_suit('hearts'), ['7', 'Q', 'K', 'A'])
+		self.assertEqual(self.hand.get_cards_by_suit('spades'), ['J'])
+		self.assertEqual(self.hand.get_cards_by_suit('diamonds'), ['A'])
+		self.assertEqual(self.hand.get_cards_by_suit('clubs'), [])	
+
 
 if __name__ == '__main__':
 	unittest.main()	
